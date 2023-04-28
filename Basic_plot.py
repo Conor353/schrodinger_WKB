66434cd2663bd3760657ec362de04477c7a217f2
Conor C <conor353@gmail.com>
2023-04-28 10:29:10 +1000

    Update Basic_plot.py

--- Basic_plot.py
+++ Basic_plot.py
@@ -2,80 +2,64 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.special as sp
 
-# linearly spaced numbers
-range = 3
-steps= 500
+# Create linearly spaced numbers
+x_range = 3
+num_steps = 500
+x = np.linspace(-x_range, x_range, num_steps)
 
-x = np.linspace(-range,range,steps)
+# Define the exact solution function
+def y_exact(omega, epsilon):
+    numerator = (sp.jv(1/6, 8/(3*epsilon)) * sp.jv(-1/6, (omega**3)/(3*epsilon))
+                 - sp.jv(-1/6, 8/(3*epsilon)) * sp.jv(1/6, (omega**3)/(3*epsilon)))
 
-#Exact solion as function
+    denominator = (sp.jv(-1/6, 1/(3*epsilon)) * sp.jv(1/6, 8/(3*epsilon))
+                   - sp.jv(-1/6, 8/(3*epsilon)) * sp.jv(1/6, 1/(3*epsilon)))
 
-def y_exact(o ,e):
-
-    numer = sp.jv(1/6,8/(3*e))*sp.jv(-1/6,(o**3)/(3*e)) - sp.jv(-1/6,8/(3*e))*sp.jv(1/6,(o**3)/(3*e))#numerator
-
-    denom = sp.jv(-1/6,1/(3*e))*sp.jv(1/6,8/(3*e)) - sp.jv(-1/6,8/(3*e))*sp.jv(1/6,1/(3*e))
-
-    D = numer/denom
-
-    y = (o**0.5)*(D)
+    D = numerator / denominator
+    y = (omega**0.5) * D
 
     return y
 
-#WKB approximatiation
+# Define the WKB approximation function
+def wkb(epsilon, omega):
+    p = epsilon
+    i = complex(0, 1)
 
-def wbk(e, o):
-    p = e
+    big_term = (1 - i) / (np.sqrt(2) * (np.exp((16 * i) / (3 * p)) - np.exp((2 * i) / (3 * p))))
 
-    i = complex(0,1)
+    c_1 = -np.exp(i / (3 * p)) * big_term
+    c_2 = np.exp((17 * i) / (3 * p)) * big_term
 
-    BIG = (1-i)/(np.sqrt(2)*(np.exp((16*i)/(3*p)) - np.exp((2*i)/(3*p))))
-
-    c_1 = -np.exp(i/(3*p))*BIG
-
-    c_2 = np.exp((17*i)/(3*p))*BIG
-
-    coef = complex(1,1)/np.sqrt(2)
-
-    exponent = complex(0,1)*(o**3)/(p*3)
+    coef = complex(1, 1) / np.sqrt(2)
+    exponent = complex(0, 1) * (omega**3) / (p * 3)
 
     expon_1 = np.exp(exponent)
-
     expon_2 = np.exp(-exponent)
 
-    y = coef*c_1*(1/o)*expon_1 + coef*c_2*(1/o)*expon_2
+    y = coef * c_1 * (1 / omega) * expon_1 + coef * c_2 * (1 / omega) * expon_2
 
     return y
 
+# Set epsilon value
+epsilon = 0.5
 
+# Calculate exact and WKB solutions
+y_exact_values = y_exact(omega=x, epsilon=epsilon)
+y_wkb_values = wkb(epsilon=epsilon, omega=x)
 
+# Calculate the relative error
+rel_error = (y_wkb_values - y_exact_values) / y_exact_values
 
-#Bessel function
-
-eps =0.5
-
-y_ex = y_exact(e=eps, o=x)
-
-y_wbk = wbk(e=eps, o=x)
-
-# relative error
-
-rel_error = (y_wbk -y_ex)/y_ex
-
-
-# setting the axes at the centre
+# Configure the plot
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-
-plt.xlim([0, range])
+plt.xlim([0, x_range])
 plt.ylim([-2, 2])
 
+# Plot the exact and WKB solutions
+plt.plot(x, y_exact_values, 'black', label=f'Exact solution with epsilon = {epsilon}')
+plt.plot(x, y_wkb_values, 'blue', label=f'WKB solution with epsilon = {epsilon}')
 
-plt.plot(x,y_ex,'black', label='Exact solution' + ' with epsilon = ' + str(eps))
-plt.plot(x,y_wbk,'blue', label='WKB solution' + ' with epsilon = ' + str(eps))
-
+# Add legend and display the plot
 plt.legend(loc='upper left')
-
-# show the plot
 plt.show()
-
